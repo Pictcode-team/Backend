39e30da966e56400216f52c23bf989054d1350c4
Ricardo Estrada <angel.sevilla@eon.com.mx>
2021-03-03 00:14:47 -0600

    Configuring workspace service running in port 8001

--- workspace-service/config/index.js
+++ workspace-service/config/index.js
@@ -1,4 +1,4 @@
 require('dotenv').config()
 module.exports = {
-  PORT: process.env.PORT
+  WORKSPACE_PORT: process.env.WORKSPACE_PORT
 }
--- workspace-service/src/delivery/http/server.js
+++ workspace-service/src/delivery/http/server.js
@@ -1,4 +1,4 @@
-const { PORT } = require('../../../config')
+const { WORKSPACE_PORT } = require('../../../config')
 const Fastify = require('fastify')
 const app = Fastify({ logger: true })
 
@@ -6,7 +6,7 @@ app.get('/', async (request, reply) => {
   return { test: 'OK' }
 })
 
-app.listen(PORT, function (err, address) {
+app.listen(WORKSPACE_PORT, '0.0.0.0', function (err, address) {
   if (err) {
     app.log.error(err)
     process.exit(1)
