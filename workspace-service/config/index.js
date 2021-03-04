60b8b4a79c6d9c5c89f5d2dddd150d01ff4da384
Axel Espinosa <axeldavid45@gmail.com>
2021-03-03 20:54:20 -0600

    chore(workspace): change gitignore and dotenv

--- workspace-service/config/index.js
+++ workspace-service/config/index.js
@@ -1,4 +1,5 @@
-require('dotenv').config()
+const { join } = require('path')
+require('dotenv').config({ path: join(__dirname, '/../../.env') })
 module.exports = {
   WORKSPACE_PORT: process.env.WORKSPACE_PORT
 }
