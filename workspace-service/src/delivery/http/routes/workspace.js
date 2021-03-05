15262417500300a589b8b8f21f1c59e3c601e65d
Axel Espinosa <axeldavid45@gmail.com>
2021-03-04 23:59:47 -0600

    feat(workspace): server receives images and validates them

--- workspace-service/src/delivery/http/routes/workspace.js
+++ workspace-service/src/delivery/http/routes/workspace.js
@@ -1,6 +1,11 @@
+const { MAX_IMAGES } = require('../../../../config')
+const multer = require('fastify-multer')
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage, fileFilter: require('../utils/file-filter') })
+
 async function workspacesRoutes (fastify, options) {
-  fastify.post('/', async (req, reply) => {
-    return { ...req.body }
+  fastify.post('/', { preHandler: upload.array('images', MAX_IMAGES) }, async (req, reply) => {
+    return { uploaded: true }
   })
 }
 
