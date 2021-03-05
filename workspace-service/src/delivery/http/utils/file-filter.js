110cf38da551a4d0ad0f646fb07ebf67bd66b6ae
Axel Espinosa <axeldavid45@gmail.com>
2021-03-05 10:53:29 -0600

    feat(workspace): refactored image upload

--- workspace-service/src/delivery/http/utils/file-filter.js
+++ workspace-service/src/delivery/http/utils/file-filter.js
@@ -1,8 +1,4 @@
-const allowedMimeTypes = ['image/gif', 'image/png', 'image/jpeg', 'image/bmp', 'image/webp', 'image/svg+xml']
+const { images: { verifyImageType } } = require('../../../useCases')
 module.exports = function fileFilter (request, file, cb) {
-  console.log(request.files)
-  if (allowedMimeTypes.includes(file.mimetype)) {
-    return cb(null, true)
-  }
-  return cb(null, false)
+  return cb(null, verifyImageType(file))
 }
