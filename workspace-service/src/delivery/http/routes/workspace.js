680a61d24d2e0d529c047aef75b29f340e4039be
Axel Espinosa <axeldavid45@gmail.com>
2021-03-08 13:46:48 -0600

    feat(workspace): upload images completed

--- workspace-service/src/delivery/http/routes/workspace.js
+++ workspace-service/src/delivery/http/routes/workspace.js
@@ -1,12 +1,15 @@
+'use strict'
 const { MAX_IMAGES } = require('../../../../config')
+const { Images } = require('../../../useCases')
+const s3Storage = require('../../../lib/s3/storage')
 const multer = require('fastify-multer')
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage, fileFilter: require('../utils/file-filter') })
 
 async function workspacesRoutes (fastify, options) {
   fastify.post('/', { preHandler: upload.array('images', MAX_IMAGES) }, async (req, reply) => {
-    console.log(req.files)
-    console.log({ ...req.body })
+    const uploadProcess = await Images.uploadImages(s3Storage())(req.files, req.body.uuid)
+    console.log(uploadProcess)
     return { uuid: '3e3e2l3.23.23-2-3-23' }
   })
 }
