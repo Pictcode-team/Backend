8ea2aac71307bc3f71eaa28165ee0023d7c001e9
Ricardo Estrada <angel.sevilla@eon.com.mx>
2021-03-08 20:07:05 -0600

    deploy(environment): Final configuration of docker-compose

--- backend_api_gateway/app/app/api/api_v1/endpoints/images.py
+++ backend_api_gateway/app/app/api/api_v1/endpoints/images.py
@@ -1,15 +1,9 @@
-from typing import List, Any
-import uuid
-from datetime import datetime
+from typing import Any
 
 from fastapi import (
     APIRouter,
-    UploadFile,
-    File,
-    HTTPException
+    Response
 )
-from fastapi.encoders import jsonable_encoder
-from fastapi import Response
 from fastapi.param_functions import Depends
 
 from app.schemas.images import ImageUploadResponse
@@ -18,18 +12,16 @@ from app.api.deps import SenderImages
 router = APIRouter()
 
 
-
-
 @router.post("/", response_model=ImageUploadResponse)
 async def upload_images(
-    # images: List[UploadFile] = File(...), 
+    # images: List[UploadFile] = File(...),
     response: Response,
     images: SenderImages = Depends(SenderImages),
-    ) -> Any:
+) -> Any:
     response_upload = images.response_upload
     response.status_code = response_upload[0]
     image_upload_response = ImageUploadResponse(
         images=images.images_names,
-        **response_upload[1], 
-        )
+        **response_upload[1],
+    )
     return image_upload_response
