3a56d4f849065145b1f63e531e76b7f89b893f5c
Axel Espinosa <axeldavid45@gmail.com>
2021-03-18 18:42:14 -0600

    feat(workspaces): migrations created and command added to package.json

---
+++ workspace-service/config/database.js
@@ -0,0 +1,27 @@
+const { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } = require('./')
+module.exports = {
+  development: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: 'postgres'
+  },
+  test: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: 'postgres'
+  },
+  production: {
+    username: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: 'postgres'
+  }
+}
