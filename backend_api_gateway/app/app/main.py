f87b82a233c6562dd434775a93ef074949997733
Ricardo Estrada <angel.sevilla@eon.com.mx>
2021-03-23 08:50:19 -0600

    Second proof headers. deploy(CORS)

--- backend_api_gateway/app/app/main.py
+++ backend_api_gateway/app/app/main.py
@@ -20,9 +20,9 @@ middleware = [
     Middleware(
         CORSMiddleware,
         allow_origins=["*"],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
+        # allow_credentials=True,
+        # allow_methods=["*"],
+        # allow_headers=["*"],
     )
 ]
 
